6201aa5497e51890862fb6ac1e10f7056012f334
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-06-24 22:27:13 +0300

    Added a Main Header & Navlinks

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,22 +1,31 @@
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import NewPlace from '../places/pages/NewPlace';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-import Users from '../users/pages/Users';
+import Users from "../users/pages/Users";
+import NewPlace from "../places/pages/NewPlace";
+import MainNavigation from "../shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
-}
+};
 
 export default App;
